6a6dbe2981c49125acd7c4bb0b1e726b9c93a058
zahidabdillahdev <zahidabdillah@protonmail.com>
2026-08-09 03:39:43 +0700

    feat: add advanced request assertions

---
+++ apps/api/prisma/migrations/20260809160000_add_request_assertions/migration.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "TestRequest" ADD COLUMN "expectedHeaderName" TEXT;
+ALTER TABLE "TestRequest" ADD COLUMN "expectedHeaderValue" TEXT;
+ALTER TABLE "TestRequest" ADD COLUMN "jsonPath" TEXT;
+ALTER TABLE "TestRequest" ADD COLUMN "expectedJsonValue" TEXT;
+ALTER TABLE "TestRequest" ADD COLUMN "maxDurationMs" INTEGER;
